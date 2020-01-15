3aa830d7e821efbd862c3d1b8582a874c4cb4766
田康 <245890367@qq.com>
2020-01-15 08:59:53 +0800

    '提交完整项目'

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -10,7 +10,6 @@
                     background="#1ee5f9"
                     placeholder="请输入编码/条码/名称/助记码"
                     show-action
-                    autofocus
                     shape="round"
                     @search="onSearch"
                 >
@@ -40,7 +39,7 @@
                             placeholder="请扫描货位架"
                         />
                     </div>
-                     <div v-if="!isShow" style="background:#35c347;" class="shelf-container">
+                    <div v-if="!isShow" style="background:#35c347;" class="shelf-container">
                         <van-field
                             ref="makeSureCart"
                             class="shelf-input"
@@ -57,7 +56,7 @@
 
 <script>
     import homeContent from "./components/homeContent";
-    import { getPhoneUpperShelfShop, upperShelfShop,isUsedCarNum } from "@/api";
+    import { getPhoneUpperShelfShop, upperShelfShop, isUsedCarNum } from "@/api";
     import { mapGetters } from "vuex";
     export default {
         name: "Home",
@@ -71,7 +70,7 @@
                 finished: false,
                 searchVal: "",
                 inputHwbh: "",
-                carNum:'',
+                carNum: "",
                 page: 1,
                 shopList: []
             };
@@ -79,15 +78,9 @@
         computed: {
             ...mapGetters(["getUsername"]),
             isShow() {
-                console.log("shopList", this.shopList);
                 if (this.shopList.length === 0) {
                     return false;
                 } else {
-                    // console.log(
-                    //     this.shopList.filter(item => {
-                    //         return item.checked;
-                    //     })
-                    // );
                     let isChecked = this.shopList.filter(item => {
                         return item.checked;
                     });
@@ -110,34 +103,28 @@
                     page: this.page
                 }).then(res => {
                     this.loading = false;
-                    console.log("res", res);
                     this.shopList.push(...res.result);
                     this.shopList.forEach((item, ind) => {
                         if (!item.hasOwnProperty("checked")) {
-                             this.$set(item, "XTM", '');
-                              this.$set(item, "SJSL", item.SL);
+                            this.$set(item, "XTM", "");
+                            this.$set(item, "SJSL", item.SL);
                             this.$set(item, "checked", false);
                         }
                     });
-                    console.log("length", res.result.length);
                     if (res.result.length < 10) {
                         this.finished = true;
-                        console.log("zhixing ");
                     }
                 });
             },
             onSearch() {
                 this.loading = true;
                 window.scrollTo(0, 0);
-               
+
                 this.loading = false;
-                console.log(this.searchVal);
-                let timer = setTimeout(()=>{
-                      this.initData();
-                      clearTimeout(timer);
-                },0)
-              
-                
+                let timer = setTimeout(() => {
+                    this.initData();
+                    clearTimeout(timer);
+                }, 0);
             },
             trimVal(val) {
                 return val.replace(/(^\s*)|(\s*$)/g, "").toUpperCase();
@@ -146,38 +133,29 @@
              * 获得选中的值
              */
             getChoosed() {
-                console.log(this.shopList);
-                console.log(
-                    this.shopList.filter(item => {
-                        return item.checked;
-                    })
-                );
                 return this.shopList.filter(item => {
                     return item.checked;
                 });
             },
             // 检查拣货篮有没有被占用
-            makeSureCart(){
-                console.log(this.carNum);
-                  let regCarNum = /^[A-Z]\d{3}$/g;
-                if(regCarNum.test(this.carNum)){
-                    isUsedCarNum({carNum:this.carNum}).then(res =>{
-                          this.carNum = '';
-                        if(res.success){
-                             this.$toast.success("该拣货篮没有被占用");
+            makeSureCart() {
+                let regCarNum = /^[A-Z]\d{3}$/g;
+                if (regCarNum.test(this.carNum)) {
+                    isUsedCarNum({ carNum: this.carNum }).then(res => {
+                        this.carNum = "";
+                        if (res.success) {
+                            this.$toast.success("该拣货篮没有被占用");
                         } else {
                             this.$toast.fail("该拣货篮被占用");
                         }
                     });
-                }else {
-                      this.$toast.success("拣货篮格式不正确");
-                      this.carNum = '';
+                } else {
+                    this.$toast.success("拣货篮格式不正确");
+                    this.carNum = "";
                 }
             },
             // 上架
             handleUpperShelf() {
-                console.log(this.inputHwbh);
-                // console.log(this.getChoosed()[0].HWBH);
                 if (this.inputHwbh) {
                     if (
                         this.trimVal(this.inputHwbh) ===
@@ -185,38 +163,36 @@
                             this.getChoosed()[0] ? this.getChoosed()[0].HWBH : ""
                         )
                     ) {
-                           let reg = /^\s*$/g || "";
-                        if(reg.test(this.getChoosed()[0].SJSL)){
-                             this.$toast.fail("上架数量不能为空！");
-                        } else{
-                        this.$dialog
-                            .confirm({
-                                title: "提示",
-                                message: "确定上架吗"
-                            })
-                            .then(() => {
-                                // on confirm
-                                upperShelfShop({
-                                    shopInfo: this.getChoosed()[0],
-                                    username: this.getUsername
-                                }).then(res => {
-                                    console.log(res);
-                                    if (res.success) {
-                                        this.$toast.success("上架成功");
-                                        this.inputHwbh = "";
-                                        this.searchVal = "";
-                                        this.onSearch();
-                                        // this.reload();
-                                    } else {
-                                        this.$toast.fail("上架失败");
-                                    }
+                        let reg = /^\s*$/g || "";
+                        if (reg.test(this.getChoosed()[0].SJSL)) {
+                            this.$toast.fail("上架数量不能为空！");
+                        } else {
+                            this.$dialog
+                                .confirm({
+                                    title: "提示",
+                                    message: "确定上架吗"
+                                })
+                                .then(() => {
+                                    // on confirm
+                                    upperShelfShop({
+                                        shopInfo: this.getChoosed()[0],
+                                        username: this.getUsername
+                                    }).then(res => {
+                                        if (res.success) {
+                                            this.$toast.success("上架成功");
+                                            this.inputHwbh = "";
+                                            this.searchVal = "";
+                                            // this.onSearch();
+                                            this.reload();
+                                        } else {
+                                            this.$toast.fail("上架失败");
+                                        }
+                                    });
+                                })
+                                .catch(() => {
+                                    // on cancel
                                 });
-                            })
-                            .catch(() => {
-                                // on cancel
-                            });
-                        } 
-                    
+                        }
                     } else {
                         this.$toast.fail("请扫描正确的货位编号");
                         this.inputHwbh = "";
@@ -237,34 +213,34 @@
                     searchVal: this.searchVal,
                     page: this.page
                 }).then(res => {
-                    console.log(res);
                     if (res.result.length < 10) {
                         this.finished = true;
-                        console.log("zhixing ");
                     }
                     this.shopList = res.result;
                     this.$toast.clear();
                     this.shopList.forEach((item, ind) => {
-                         this.$set(item, "XTM", '');
-                          this.$set(item, "SJSL", item.SL);
+                        this.$set(item, "XTM", "");
+                        this.$set(item, "SJSL", item.SL);
                         if (ind === 0) {
                             this.$set(item, "checked", true);
                         } else {
                             this.$set(item, "checked", false);
                         }
                     });
-                     if (this.searchVal) {
-                    if (this.$refs.addShelf) {
-                        this.$refs.addShelf.focus();
+                    if (this.searchVal) {
+                        if (this.$refs.addShelf) {
+                            this.$refs.addShelf.focus();
+                        }
+                    } else {
+                        document.querySelector(".search-content input").focus();
                     }
-                } else {
-                    document.querySelector(".search-content input").focus();
-                }
                 });
             }
         },
         mounted() {
-            this.initData();
+            document.querySelector(".header-content .van-field__control").focus();
+
+            // this.initData();
         }
     };
 </script>
