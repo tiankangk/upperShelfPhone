71ed1918b1a86ac7f4d7dd106418a675c210a537
tiankang <245890367@qq.com>
2020-02-19 17:55:19 +0800

    调整输入框清除图标的字体

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 if (process.env.NODE_ENV === 'development') {
-    var baseURL = `http://192.168.0.71:3001/`
+    var baseURL = `http://192.168.0.204:3001/`
     
 } else {
     var baseURL = `http://192.168.0.38:3001/`
